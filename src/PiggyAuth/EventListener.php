acfa22c746f297be0faa5298629bad62bf24fcc2
Mr. Piggy <mcpepig123@gmail.com>
2016-07-28 15:45:23 -0700

    Yay?

--- src/PiggyAuth/EventListener.php
+++ src/PiggyAuth/EventListener.php
@@ -1,7 +1,21 @@
 <?php
 namespace PiggyAuth;
 
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\inventory\InventoryPickupArrowEvent;
+use pocketmine\event\inventory\InventoryPickupItemEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerExhaustEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\Listener;
 
 class EventListener implements Listener {
@@ -9,30 +23,113 @@ class EventListener implements Listener {
         $this->plugin = $plugin;
     }
 
+    public function onBreak(BlockBreakEvent $event) {
+        $player = $event->getPlayer();
+        if(!$this->plugin->isAuthenticated($player)) {
+            $event->setCancelled();
+        }
+    }
+
+    public function onPlace(BlockPlaceEvent $event) {
+        $player = $event->getPlayer();
+        if(!$this->plugin->isAuthenticated($player)) {
+            $event->setCancelled();
+        }
+    }
+
+    public function onChat(PlayerChatEvent $event) {
+        $player = $event->getPlayer();
+        $message = $event->getMessage();
+        if(!$this->plugin->isAuthenticated($player)) {
+            if($this->plugin->getConfig("chat-login")) {
+                if($this->plugin->isRegistered($player->getName())) {
+                    $this->plugin->login($player, $message);
+                } else {
+                    if(strlen($message) < $this->plugin->getConfig()->get("minimum-password-length")) {
+                        $player->sendMessage($this->plugin->getConfig()->get("password-too-short"));
+                    } else {
+                        if(!isset($this->plugin->confirmPassword[strtolower($player->getName())])) {
+                            $this->plugin->confirmPassword[strtolower($player->getName())] = $message;
+                            $player->sendMessage($this->plugin->getConfig("confirm-password"));
+                        } else {
+                            if($this->plugin->confirmPassword[strtolower($player->getName())] == $message) {
+                                $this->plugin->register($player, $message);
+                                unset($this->plugin->confirmPassword[strtolower($player->getName())]);
+                            } else {
+                                $player->sendMessage($this->plugin->getConfig("password-not-same"));
+                                unset($this->plugin->confirmPassword[strtolower($player->getName())]);
+                            }
+                        }
+                    }
+                }
+            }
+            $event->setCancelled();
+        }
+    }
+
+    public function onCommandPreprocess(PlayerCommandPreprocessEvent $event) {
+        $player = $event->getPlayer();
+        $message = strtolower($event->getMessage());
+        if(!$this->plugin->isAuthenticated($player)) {
+            if($message !== "/login" && $message !== "register"){
+               $event->setCancelled(); 
+            }
+        }
+    }
+    
+    public function onDrop(PlayerDropItemEvent $event) {
+        $player = $event->getPlayer();
+        if(!$this->plugin->isAuthenticated($player)) {
+            $event->setCancelled();
+        }
+    }
+
+    public function onExhaust(PlayerExhaustEvent $event) {
+        $player = $event->getPlayer();
+        if(!$this->plugin->isAuthenticated($player)) {
+            $event->setCancelled();
+        }
+    }
+    
     public function onInteract(PlayerInteractEvent $event) {
         $player = $event->getPlayer();
-        if($this->plugin->isAuthenticated($player)){
-            if(!$this->plugin->getConfig()->get("allow-block-interaction")){
-                $event->setCancelled();
+        if(!$this->plugin->isAuthenticated($player)) {
+            $event->setCancelled();
+        }
+    }
+
+    public function onConsume(PlayerItemConsumeEvent $event) {
+        $player = $event->getPlayer();
+        if(!$this->plugin->isAuthenticated($player)) {
+            $event->setCancelled();
+        }
+    }
+    
+    public function onJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        if($this->plugin->getConfig("auto-authentication")) {
+            $data = $this->plugin->getPlayer($player->getName());
+            if(!is_null($data)) {
+                if($player->getUniqueId() == $data["uuid"]){
+                    $this->plugin->forcelogin($player);
+                }
             }
         }
     }
 
     public function onMove(PlayerMoveEvent $event) {
         $player = $event->getPlayer();
-        if($this->plugin->isAuthenticated($player)){
-            if(!$this->plugin->getConfig()->get("allow-movement")){
+        if(!$this->plugin->isAuthenticated($player)) {
+            if(!$this->plugin->getConfig()->get("allow-movement")) {
                 $event->setCancelled();
             }
         }
     }
 
-    public function onJoin(PlayerJoinEvent){
-
-    }
-
-    public function onQuit(PlayerQuitEvent){
-
+    public function onQuit(PlayerQuitEvent $event) {
+        if($this->plugin->isAuthenticated($player)) {
+            unset($this->plugin->authenticated[strtolower($player->getName())]);
+        }
     }
 
 }
--- src/PiggyAuth/Main.php
+++ src/PiggyAuth/Main.php
@@ -5,34 +5,129 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
 
 class Main extends PluginBase {
+    public $authenticated;
+    public $confirmPassword;
+
     public function onEnable() {
+        if(!file_exists($this->getDataFolder() . "players.db")) {
+            $this->db = new \SQLite3($this->getDataFolder() . "players.db", SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE);
+            $this->db->exec("CREATE TABLE players (name TEXT PRIMARY KEY, password TEXT, uuid INT);");
+        } else {
+            $this->db = new \SQLite3($this->getDataFolder() . "players.db", SQLITE3_OPEN_READWRITE);
+        }
         $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         $this->getLogger()->info("§aEnabled.");
     }
-    
-    public function isCorrectPassword(Player $player, $password){
-        
+
+    public function getPlayer($player) {
+        $statement = $this->db->prepare("SELECT * FROM players WHERE name = :name");
+        $statement->bindValue(":name", strtolower($player->getName()), SQLITE3_TEXT);
+        $result = $statement->execute();
+        if($result instanceof \SQLite3Result) {
+            $data = $result->fetchArray(SQLITE3_ASSOC);
+            $result->finalize();
+            if(isset($data["name"]) and $data["name"] === $name) {
+                unset($data["name"]);
+                $statement->close();
+                return $data;
+            }
+        }
+        $statement->close();
+        return null;
     }
-    
-    public function isAuthenticated(Player $player){
-        
+
+    public function updatePlayer(Player $player){
+        $statement = $this->db->prepare("UPDATE players SET uuid = :uuid WHERE name = :name");
+            $statement->bindValue(":name", strtolower($player->getName()), SQLITE3_TEXT);
+            $statement->bindValue(":uuid", $player->getUniqueId(), SQLITE3_INTEGER);
+            $statement->execute();
     }
-   
-   public function isRegistered(Player $player){
-        
+
+    public function isCorrectPassword(Player $player, $password) {
+        $data = $this->getPlayer($player->getName());
+        if(!is_null($data)) {
+            if(password_verify($password, $data["password"])) {
+                return true;
+            }
+        }
+        return false;
     }
-     
-    public function login(Player $player, $password){
-        
+
+    public function isAuthenticated(Player $player) {
+        if(isset($this->authenticated[strtolower($player->getName())])) return true;
+        return false;
     }
-    
-    public function register(Player $player, $password){
-        
+
+    public function isRegistered($player) {
+        return $this->getPlayer($player) !== null;
     }
-    
-    public function changepassword(Player $player, $oldpassword, $newpassword){
-        
+
+    public function login(Player $player, $password) {
+        if($this->isAuthenticated($player)) {
+            $player->sendMessage($this->getConfig()->get("already-authenticated"));
+            return false;
+        }
+        if(!$this->isRegistered($player->getName())) {
+            $player->sendMessage($this->getConfig()->get("not-registered"));
+            return false;
+        }
+        if(!$this->isCorrectPassword($player, $password)) {
+            $player->sendMessage($this->getConfig()->get("incorrect-password"));
+            return false;
+        }
+        $this->forcelogin($player);
+        return true;
     }
-    
+
+    public function forcelogin(Player $player) {
+        $this->authenticated[strtolower($player->getName())] = true;
+        $this->updatePlayer($player);
+        $player->sendMessage($this->getConfig()->get("authentication-success"));
+        return true;
+    }
+
+    public function register(Player $player, $password) {
+        if($this->isRegistered($player->getName())) {
+            $player->sendMessage($this->getConfig()->get("already-registered"));
+            return false;
+        }
+        $this->authenticated[strtolower($player->getName())];
+        $player->sendMessage($this->getConfig()->get("register-success"));
+        $statement = $this->db->prepare("INSERT INTO players (name, password, uuid) VALUES (:name, :password, :uuid)");
+        $statement->bindValue(":name", strtolower($player->getName()), SQLITE3_TEXT);
+        $statement->bindValue(":password", password_hash($password, PASSWORD_BCRYPT), SQLITE3_TEXT);
+        $statement->bindValue(":uuid", $player->getUniqueId(), SQLITE3_INTEGER);
+        $statement->execute();
+        return true;
+    }
+
+    public function changepassword(Player $player, $oldpassword, $newpassword) {
+        if(!$this->isRegistered($player->getName())) {
+            $player->sendMessage($this->getConfig()->get("not-registered"));
+            return false;
+        }
+        if(!$this->isCorrectPassword($player, $oldpassword)) {
+            $player->sendMessage($this->getConfig()->get("incorrect-password"));
+            return false;
+        }
+        $statement = $this->db->prepare("UPDATE players SET password = :password WHERE name = :name");
+            $statement->bindValue(":name", strtolower($player->getName()), SQLITE3_TEXT);
+            $statement->bindValue(":password", password_hash($password, PASSWORD_BCRYPT), SQLITE3_TEXT);
+            $statement->execute();
+    }
+
+    public function resetpassword($player) {
+        if($this->isRegistered($player)) {
+            $statement = $this->db->prepare("DELETE FROM players WHERE name = :name");
+            $statement->bindValue(":name", strtolower($player), SQLITE3_TEXT);
+            $statement->execute();
+            if(isset($this->authenticated[strtolower($player)])) {
+                unset($this->authenticated[strtolower($player)]);
+            }
+            return true;
+        }
+        return false;
+    }
+
 }
